0fe205d1776fda645a5d08f8799f2b8d11f55db3
Rupsha Nandi <shirihimesama109@gmail.com>
2025-08-23 21:58:54 +0530

    CSP added.

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,13 +13,22 @@ import authRoutes from './routes/authRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import path from "path";
 
 const app = express();
+app.use(helmet({
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https:", "data:"], // Changed from "*" to "https:" for better security
+    },
+  }
+}));
 
 // ✅ Middleware stack
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
 app.use(morgan('dev'));
@@ -35,6 +44,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
+app.use(express.static(path.join(process.cwd(), "dist")));
 
 // ✅ Error handling middleware
 app.use(errorHandler);
